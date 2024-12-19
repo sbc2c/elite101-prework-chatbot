c97ccb413e2363a43a578926254005754813f2fa
sbc2c <sreeharshab2010@gmail.com>
2024-12-19 16:43:18 -0600

    main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 print('Hello, I am your Robo Friend')
 name = input("What is your name friend? ")
 age = input("And how old are you " + name + " .")
+print('I am Robo-295, but you can just call me robo')
